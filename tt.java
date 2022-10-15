c13c42b6e610b1d14d0346a8d6c1f553457a4318
KittenCN <owdely@vip.163.com>
2022-10-15 10:23:08 +0800

    new project

--- tt.java
+++ tt.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
-public class tt{
-    public static void main(String[] args){
-        StringBuilder sb = new StringBuilder();
-        sb.append("str");
-        sb.append("[]".repeat(2));
-        System.out.println(sb.toString());
+    public class A extends B{
+        public String toString(){
+            return "A";
+        }
     }
+    public class B{
+        public String toString(){
+            return "B";
+        }
+    }
+    public class tt{
+        public static void main(String[] args){
+            B a = new A();
+            A b = new A();
+            System.out.println(b);
+            System.out.println(a);
+        }
 }
