59e5c9bd8364306cfe07355ee1f8626a01fb7812
KittenCN <owdely@vip.163.com>
2022-11-19 13:35:00 +0800

    modify

--- temp/test.java
+++ temp/test.java
@@ -7,16 +7,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class test {
+    static void foo(int length){
+        stack.push(1);
+        
+    }
+    static void foo(int length, String seq){
+        if(seq.length()==length){
+            System.out.printf("%s \n",seq);
+            return;
+        }
+        String seq0 = seq + "0";
+        foo(length, seq0);
+        if(seq.length() != length -1){
+            String seq1 = seq + "1";
+            foo(length, seq1);
+        }
+    }
     public static void main(String[] args) {
-        foo [] list = new foo[5]; 
-
-        list[0] = new foo(2,-3);
-        list[1] = new foo(2,13);
-        list[2] = new foo(2,5);
-        list[3] = new foo(2,1);
-        list[4] = new foo(2,-20);
-
-        Arrays.sort(list);
-        System.out.println( Arrays.toString(list) ); 
+        
+       foo(4);
     }
 }
