3ec1e9c3ede175e4e886358601e93e66c52aeadd
KittenCN <owdely@vip.163.com>
2022-11-22 09:51:42 +0800

    ap csa 1

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sa = new Scanner(System.in);
+        double a,b,c;
+        a=sa.nextDouble();
+        b=2;
+        c=a+b;
+        System.out.print(c);
+        sa.close();
+    }
+}
---
+++ temp/test3.java
@@ -0,0 +1,6 @@
+package temp;
+public class test3 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
