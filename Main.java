0b84ac024efbcb6953135650595da38242ff8577
KittenCN <owdely@vip.163.com>
2023-03-29 13:08:04 +0800

    update something

--- Main.java
+++ Main.java
@@ -1,48 +1,12 @@
-import java.util.*;
-
-public class Main{
-    public static void main(String[] args){
-        show(new cat());
-        // show(new dog());
-        // show(new Animal());
-
-        // Animal a = new dog();
-        // a.eat();
-        // cat c = (cat)a;
-        // c.eat();
-    }
-    public static void show(Animal ani){
-        ani.eat();
-        if (ani instanceof dog){
-            dog d = (dog)ani;
-            d.work();
-        }
-        if (ani instanceof cat){
-            cat c = (cat)ani;
-            c.work();
-        }
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        String st1 = "1";
+        String st2 = new String("2");
+        int st3 = 3;
+        System.out.println(st1 + st2 + st3);
+        
     }
 }
-class Animal{
-    void eat(){
-        System.out.println("I am eating");
-    }
-}
-
-class dog extends Animal{
-    public void eat(){
-        System.out.println("I am a dog, I am eating");
-    }
-    public void work(){
-        System.out.println("I am a dog, I am working");
-    }
-}
-
-class cat extends Animal{
-    public void eat(){
-        System.out.println("I am a cat, I am eating");
-    }
-    public void work(){
-        System.out.println("I am a cat, I am working");
-    }
-}
+ 
--- test.java
+++ test.java
@@ -1,47 +1,10 @@
-import java.util.*;
-
-public class test{
-    public static void main(String[] args){
-        show(new cat());
-        show(new dog());
-
-        Animal a = new dog();
-        a.eat();
-        cat c = (cat)a;
-        c.eat();
-    }
-    public static void show(Animal ani){
-        ani.eat();
-        if (ani instanceof dog){
-            dog d = (dog)ani;
-            d.work();
-        }
-        if (ani instanceof cat){
-            cat c = (cat)ani;
-            c.work();
-        }
+import java.util.Scanner;
+ 
+public class test {
+    public static void main(String[] args) {
+       String st = "hello";
+       Class currClass = st.getClass();
+       System.out.println(currClass.getName());
     }
 }
-class Animal{
-    void eat(){
-        System.out.println("I am eating");
-    }
-}
-
-class dog extends Animal{
-    public void eat(){
-        System.out.println("I am a dog, I am eating");
-    }
-    public void work(){
-        System.out.println("I am a dog, I am working");
-    }
-}
-
-class cat extends Animal{
-    public void eat(){
-        System.out.println("I am a cat, I am eating");
-    }
-    public void work(){
-        System.out.println("I am a cat, I am working");
-    }
-}
+ 
