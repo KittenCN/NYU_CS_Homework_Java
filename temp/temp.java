060b3f7b4b22aa20883772b87ea8185a5924207e
KittenCN <owdely@vip.163.com>
2025-07-18 06:46:23 +0800

    update

--- temp/temp.java
+++ temp/temp.java
@@ -1,1 +1,25 @@
 package temp;
+import java.util.*;
+public class temp {
+    public static void main(String[] args) {
+        int[][] mat = new int[99][99];
+        int m, n;
+        Scanner sc = new Scanner(System.in);
+        m = sc.nextInt();
+        n = sc.nextInt();
+        sc.close();
+
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                mat[i][j] = i + 1;
+            }
+        }
+        for (int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                System.out.print(mat[i][j]);
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+    }
+}
